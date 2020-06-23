21fb752ba980474493541277815fd1f287811450
HJ2007 <hasinthajayasinghe07@gmail.com>
2020-06-23 20:48:58 +0530

    First commit

--- client.py
+++ client.py
@@ -1,3 +1,11 @@
 import socket
 
-server = ''
+server = 'https://socketserverhj.herokuapp.com/'
+port = 2007
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server, port))
+
+recieved = client.recv(1029).decode('utf-8')
+
+print(recieved)
